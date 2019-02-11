7693870c212785e301533d467e778a425948b56e
williamdong <dzy1997@hotmail.com>
2019-02-10 23:52:39 -0500

    up to challenge

--- project1.py
+++ project1.py
@@ -12,8 +12,124 @@ from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn import metrics
 from matplotlib import pyplot as plt
 
-from helper import *
+def load_data(fname):
+    """
+    Reads in a csv file and return a dataframe. A dataframe df is similar to dictionary.
+    You can access the label by calling df['label'], the content by df['content']
+    the rating by df['rating']
+    """
+    return pd.read_csv(fname)
 
+
+def get_split_binary_data():
+    """
+    Reads in the data from data/dataset.csv and returns it using
+    extract_dictionary and generate_feature_matrix split into training and test sets.
+    The binary labels take two values:
+        -1: poor/average
+         1: good
+    Also returns the dictionary used to create the feature matrices.
+    """
+    fname = "data/dataset.csv"
+    dataframe = load_data(fname)
+    dataframe = dataframe[dataframe['label'] != 0]
+    positiveDF = dataframe[dataframe['label'] == 1].copy()
+    negativeDF = dataframe[dataframe['label'] == -1].copy()
+    X_train = pd.concat([positiveDF[:500], negativeDF[:500]]).reset_index(drop=True).copy()
+    dictionary = extract_dictionary(X_train)
+    X_test = pd.concat([positiveDF[500:700], negativeDF[500:700]]).reset_index(drop=True).copy()
+    Y_train = X_train['label'].values.copy()
+    Y_test = X_test['label'].values.copy()
+    X_train = generate_feature_matrix(X_train, dictionary)
+    X_test = generate_feature_matrix(X_test, dictionary)
+
+    return (X_train, Y_train, X_test, Y_test, dictionary)
+
+
+def get_imbalanced_data(dictionary):
+    """
+    Reads in the data from data/imbalanced.csv and returns it using
+    extract_dictionary and generate_feature_matrix as a tuple
+    (X_train, Y_train) where the labels are binary as follows
+        -1: poor/average
+        1: good
+    Input:
+        dictionary: the dictionary created via get_split_binary_data
+    """
+    fname = "data/imbalanced.csv"
+    dataframe = load_data(fname)
+    dataframe = dataframe[dataframe['label'] != 0]
+    positiveDF = dataframe[dataframe['label'] == 1].copy()
+    negativeDF = dataframe[dataframe['label'] == -1].copy()
+    dataframe = pd.concat([positiveDF[:300], negativeDF[:700]]).reset_index(drop=True).copy()
+    X_train = generate_feature_matrix(dataframe, dictionary)
+    Y_train = dataframe['label'].values.copy()
+
+    return (X_train, Y_train)
+
+
+def get_imbalanced_test(dictionary):
+    """
+    Reads in the data from data/dataset.csv and returns a subset of it
+    reflecting an imbalanced test dataset
+        -1: poor/average
+        1: good
+    Input:
+        dictionary: the dictionary created via get_split_binary_data
+    """
+    fname = "data/dataset.csv"
+    dataframe = load_data(fname)
+    dataframe = dataframe[dataframe['label'] != 0]
+    positiveDF = dataframe[dataframe['label'] == 1].copy()
+    negativeDF = dataframe[dataframe['label'] == -1].copy()
+    X_test = pd.concat([positiveDF[:400], negativeDF[:100]]).reset_index(drop=True).copy()
+    Y_test = X_test['label'].values.copy()
+    X_test = generate_feature_matrix(X_test, dictionary)
+
+    return (X_test, Y_test)
+
+
+def get_multiclass_training_data():
+    """
+    Reads in the data from data/dataset.csv and returns it using
+    extract_dictionary and generate_feature_matrix as a tuple
+    (X_train, Y_train) where the labels are multiclass as follows
+        -1: poor
+         0: average
+         1: good
+    Also returns the dictionary used to create X_train.
+    """
+    fname = "data/dataset.csv"
+    dataframe = load_data(fname)
+    dictionary = extract_dictionary(dataframe)
+    X_train = generate_feature_matrix(dataframe, dictionary)
+    Y_train = dataframe['label'].values.copy()
+
+    return (X_train, Y_train, dictionary)
+
+
+def get_heldout_reviews(dictionary):
+    """
+    Reads in the data from data/heldout.csv and returns it as a feature
+    matrix based on the functions extract_dictionary and generate_feature_matrix
+    Input:
+        dictionary: the dictionary created by get_multiclass_training_data
+    """
+    fname = "data/heldout.csv"
+    dataframe = load_data(fname)
+    X = generate_feature_matrix(dataframe, dictionary)
+    return X
+
+
+def generate_challenge_labels(y, uniqname):
+    """
+    Takes in a numpy array that stores the prediction of your multiclass
+    classifier and output the prediction to held_out_result.csv. Please make sure that
+    you do not change the order of the ratings in the heldout dataset since we will
+    this file to evaluate your classifier.
+    """
+    pd.Series(np.array(y)).to_csv(uniqname+'.csv', header=['label'], index=False)
+    return
 
 def select_classifier(penalty='l2', c=1.0, degree=1, r=0.0, class_weight='balanced'):
     """
@@ -109,8 +225,8 @@ def cv_performance(clf, X, y, k=5, metric="accuracy"):
     #HINT: You may find the StratifiedKFold from sklearn.model_selection
     #to be useful
     skf = StratifiedKFold(n_splits=k)
+    skf.get_n_splits(X,y)
     for train_index, test_index in skf.split(X, y):
-        #print("TRAIN:", train_index, "TEST:", test_index)
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         clf.fit(X_train,y_train)
@@ -149,11 +265,11 @@ def select_param_linear(X, y, k=5, metric="accuracy", C_range = [], penalty='l2'
     for c in C_range:
         clf = select_classifier(penalty=penalty, c=c)
         perf = cv_performance(clf, X, y, k=5, metric=metric)
+        print(c, perf)
         if perf>maxperf:
             maxperf=perf
             maxc=c
-
-return maxc
+    return maxc
 
 
 def plot_weight(X,y,penalty,metric="",C_range=[]):
@@ -246,18 +362,23 @@ def performance(y_true, y_pred, metric="accuracy"):
     # This is an optional but very useful function to implement.
     # See the sklearn.metrics documentation for pointers on how to implement
     # the requested metrics.
-    if metric=="accuracy":
-        return metrics.accuracy_score(y_true,y_pred)
-    if metric=="f1-score":
-        return metrics.f1_score(y_true,y_pred)
+
     if metric=="auroc":
         return metrics.roc_auc_score(y_true,y_pred)
-    if metric=="precision":
-        return metrics.precision_score(y_true,y_pred)
+    m=metrics.confusion_matrix(y_true,y_pred)
+    tp,fn,fp,tn=m[0,0],m[0,1],m[1,0],m[1,1]
+    if metric=="accuracy":
+        return (tp+tn)/(tp+tn+fp+tn)
+    if metric=="f1-score":
+        pre=tp/(tp+fp)
+        sen=tp/(tp+fn)
+        return 2*pre*sen/(pre+sen)
     if metric=="sensitivity":
-        return metrics.sensitivity_score(y_true,y_pred)
+        return tp/(tp+fn)
+    if metric=="precision":
+        return tp/(tp+fp)
     if metric=="specificity":
-        return metrics.specificity_score(y_true,y_pred)
+        return tn/(tn+fp)
 
 
 def main():
@@ -285,7 +406,7 @@ def main():
         score=cv_performance(clf, X_train, y_train, metric=me)
         print("C=",maxc,"is optimal under",me,"metric, cv_perf=",score)
         if me=="auroc":
-            selected_C=c
+            selected_C=maxc
     
     #q3.1(d)
     clf=select_classifier(c=selected_C)
@@ -293,15 +414,15 @@ def main():
     y_pred = clf.decision_function(X_test)
     auroc_score=performance(y_test, y_pred, metric="auroc")
     print("q3.1(d) Choose C which maximizes AUROC")
-    print("The AUROC score is:"auroc_score)
+    print("The AUROC score is:",auroc_score)
     y_pred = clf.predict(X_test)
     for me in metrics:
         if me!="auroc":
             score=performance(y_test, y_pred, metric=me)
             print("The",me,"score is",score)
 
-#q3.1(e)
-plot_weight(X_train, y_train, "l2", C_range=C_range)
+    #q3.1(e)
+    plot_weight(X_train, y_train, "l2", C_range=C_range)
 
     #q3.1(f)
     clf = select_classifier(c=0.1)
@@ -316,17 +437,18 @@ plot_weight(X_train, y_train, "l2", C_range=C_range)
         for word, index in dictionary_binary.items():
             if index==ind:
                 minwords.append(word)
-print("Most negative words")
-for i in range(4):
-    print(clf.coef_[min_ind4[i]], minwords[i])
+    print("Most negative words")
+    for i in range(4):
+        print(clf.coef_[min_ind4[i]], minwords[i])
+    
     
     for ind in max_ind4:
         for word, index in dictionary_binary.items():
             if index==ind:
                 maxwords.append(word)
-print("Most positive words")
-for i in range(4):
-    print(clf.coef_[max_ind4[i]], maxwords[i])
+    print("Most positive words")
+    for i in range(4):
+        print(clf.coef_[max_ind4[i]], maxwords[i])
     
     #q3.2(a)
     r_range=[1e-3, 1e-2, 0.1, 1, 10, 100, 1000]
@@ -338,12 +460,12 @@ for i in range(4):
     print("q3.2(a)")
     print("C=",maxc,"r=",maxr,"is optimal")
 
-#q3.2(b)
-cr_range=[]
-for i in range(25):
-    lgc=random.uniform(-3,3)
-    lgr=random.uniform(-3,3)
-    cr_range.append([10**lgc, 10**lgr])
+    #q3.2(b)
+    cr_range=[]
+    for i in range(25):
+        lgc=random.uniform(-3,3)
+        lgr=random.uniform(-3,3)
+        cr_range.append([10**lgc, 10**lgr])
     [maxc,maxr]=select_param_quadratic(X_train, y_train, param_range=cr_range)
     print("q3.2(b)")
     print("C=",maxc,"r=",maxr,"is optimal")
@@ -390,10 +512,10 @@ for i in range(25):
             perf=performance(IMB_test_labels,y_pred,metric=me)
             print("The",me,"score is",score)
 
-#q4.3(a) choose auroc
-W_range=[-2,-1.5,-1,-0.5,0,0.5,1,1.5,2]
-W_range=[10**w for w in W_range]
-maxwn=0
+    #q4.3(a) choose auroc
+    W_range=[-2,-1.5,-1,-0.5,0,0.5,1,1.5,2]
+    W_range=[10**w for w in W_range]
+    maxwn=0
     maxwp=0
     maxperf=0
     for Wn in W_range:
@@ -404,13 +526,12 @@ maxwn=0
                 maxperf=perf
                 maxwn=Wn
                 maxwp=Wp
-print("q4.3(a) Wn=",maxwn,"Wp=",maxwp,"is optimal")
-print("performance is:",maxperf)
+    print("q4.3(a) Wn=",maxwn,"Wp=",maxwp,"is optimal")
+    print("performance is:",maxperf)
 
-clf = select_classifier(c=1, class_weight={-1: 100, 1:100})
-perf=cv_performance(clf, IMB_features, IMB_labels, metric="auroc")
-print(perf)
-
+    clf = select_classifier(c=1, class_weight={-1: 100, 1:100})
+    perf = cv_performance(clf,IMB_features,IMB_labels,metric="auroc")
+                        
     #q4.3(b)
     print("q4.3(b)")
     print("The auroc score is",maxperf)
@@ -422,20 +543,20 @@ print(perf)
             perf=performance(IMB_test_labels, y_pred, metric=me)
             print("The",me,"score is",perf)
 
-#q4.4
-y_pred=clf.predict(IMB_test_features)
+    #q4.4
+    y_pred=clf.predict(IMB_test_features)
 
     clf1=select_classifier(c=1, class_weight={-1: 1,1 :1})
     clf1.fit(IMB_features, IMB_labels)
     y_pred1=clf1.predict(IMB_test_features)
     
     
-    # Read multiclass data
+    # Read multiclass dataange = C_range
     # TODO: Question 5: Apply a classifier to heldout features, and then use
     #       generate_challenge_labels to print the predicted labels
     multiclass_features, multiclass_labels, multiclass_dictionary = get_multiclass_training_data()
     heldout_features = get_heldout_reviews(multiclass_dictionary)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
